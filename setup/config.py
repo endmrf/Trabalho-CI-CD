3379598deca59c3f1eaa46132acb9003dce91ad9
endryo <endryofeitosa@gmail.com>
2024-07-01 23:07:57 -0300

    adjust gitignore

--- setup/config.py
+++ setup/config.py
@@ -3,5 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///mock_data.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'mock_data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
